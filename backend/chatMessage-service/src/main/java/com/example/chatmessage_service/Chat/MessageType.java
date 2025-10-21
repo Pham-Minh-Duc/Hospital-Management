a30789efb1609ed9d68e230a90f065dc2cd38078
Phạm Minh Đức <minhduc5116@gmail.com>
2025-10-21 23:44:35 +0700

    prepare websocket in backend

---
+++ backend/chatMessage-service/src/main/java/com/example/chatmessage_service/Chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.example.chatmessage_service.Chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
--- webAdmin/src/app/dashboard/tab/note/page.tsx
+++ webAdmin/src/app/dashboard/tab/note/page.tsx
@@ -1,86 +1,12 @@
 'use client';
 
-import React, { useEffect, useState } from "react";
-import SockJS from "sockjs-client";
-import { Client, IMessage } from "@stomp/stompjs";
-
 const NotificationPage = () => {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [input, setInput] = useState("");
-  const [clientId, setClientId] = useState("clientA");
-  const [stompClient, setStompClient] = useState<Client | null>(null);
-
-  useEffect(() => {
-    const client = new Client({
-  webSocketFactory: () => new SockJS("http://localhost:8085/ws-sockjs"), // ✅ đúng endpoint
-  connectHeaders: {
-    userId: "admin",
-  },
-  reconnectDelay: 5000,
-  onConnect: () => {
-    console.log("✅ Admin connected");
-    client.subscribe("/user/queue/messages", (message: IMessage) => {
-      setMessages((prev) => [...prev, "Client: " + message.body]);
-    });
-  },
-  onStompError: (frame) => {
-    console.error("❌ Lỗi STOMP:", frame);
-  },
-});
-
-    client.activate();
-    setStompClient(client);
-
-    return () => {
-      client.deactivate();
-    };
-  }, []);
-
-  const sendMessageToClient = () => {
-    if (stompClient && stompClient.connected) {
-      stompClient.publish({
-        destination: "/app/send-to-client",
-        body: JSON.stringify({
-          clientId,
-          content: input,
-        }),
-      });
-
-      setMessages((prev) => [
-        ...prev,
-        `Tôi (Admin) → ${clientId}: ${input}`,
-      ]);
-      setInput("");
-    }
-  };
-
-  return (
-    <div style={{ padding: "1rem" }}>
-      <h2>Admin Chat</h2>
-      <div style={{ marginBottom: "0.5rem" }}>
-        <label>Gửi tới clientId: </label>
-        <input
-          type="text"
-          value={clientId}
-          onChange={(e) => setClientId(e.target.value)}
-        />
-      </div>
-
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Nhập tin nhắn..."
-      />
-      <button onClick={sendMessageToClient} className="cursor-pointer">Gửi</button>
-
-      <ul style={{ marginTop: "1rem" }}>
-        {messages.map((msg, idx) => (
-          <li key={idx}>{msg}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  
+  return(
+    <>
+      <div>hello</div>
+    </>
+  )
 };
 
 export default NotificationPage;
